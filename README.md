597ec2e316615bc9cf94443592cc4c15f3362b88
Anthony Campos <30614274+anthonydevelops@users.noreply.github.com>
2018-10-03 13:36:52 -0700

    Initial files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ultimate Bravery
+
+Electron app for League of Legends that recommends various random builds for you to experiment with in-game.
+
+### Install & setup locally
+
+```javascript
+// In your command line, do:
+git clone 'https://github.com/anthonydevelops/ultimate-bravery.git'
+cd ultimate-bravery/
+npm install
+
+// Lastly, to fire up the application, do:
+npm start
+```
---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+// Targeting windows ONLY
+let win;
+
+function createWindow() {
+  // Create browser window
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  // Load index.html
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // Open Chrome DevTools
+  win.webContents.openDevTools();
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+// Run createWindow once app is loaded
+app.on("ready", createWindow);
+
+// Quit when all windows are closed
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
